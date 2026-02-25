4df099799eeea5f9d15fdaf763ebdd8feff1798b
abhishek <abhishekspps825406@gmail.com>
2026-02-25 22:04:33 +0530

    Final structure is ready but it has coupling in it as email and inventory update service is tightly coupled to the orderController service

--- Day9-EventDriven-architecture/app.js
+++ Day9-EventDriven-architecture/app.js
@@ -1,14 +1,18 @@
 import express from "express";
-import { OrderController } from "./orderController";
+import { OrderController } from "./orderController.js";
+import { OrderService } from "./orderService.js";
+import { EmailService } from "./emailService.js";
+import { InventoryService } from "./inventoryService.js";
 
 const app = express();
 app.use(express.json());
 
 
 /* Dependency initialization */
-
-const orderController = new OrderController();
-
+const emailService = new EmailService();
+const inventoryService = new InventoryService();
+const orderService = new OrderService(emailService,inventoryService);
+const orderController = new OrderController(orderService);
 
 /* Routes */
 
--- Day9-EventDriven-architecture/orderController.js
+++ Day9-EventDriven-architecture/orderController.js
@@ -1,9 +1,11 @@
 export class OrderController{
-
+    constructor(orderService) {
+        this.orderService = orderService;
+    }
     create(req,res) {
         // Inital processing
 
-        
+        const order = this.orderService.createOrder(req.body);
         return res.json({message:"Order created!"}); 
     }
 }
